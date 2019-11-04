fb1c450858280eb49c97c69ddcb226e3519d0601
Jean-xavier RELIGIEUX <jereligi@e2r7p1.42.fr>
2019-11-04 10:43:15 +0100

    :white_check_mark: test : update main_multi_fd

---
+++ test_files/main_multi_fd.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "../get_next_line.h"
+
+int		main(void)
+{
+	int		fd1;
+	int		fd2;
+	int		i;
+	int		n;
+	int		c;
+	char	*line;
+
+	i = 1;
+	n = 1;
+	c = 1;
+	fd1 = open("./test_files/05_file_short_lines_without_nl_at_end", O_RDONLY);
+	if (fd1 == -1)
+	{
+		printf("error open()");
+		return (0);
+	}
+	fd2 = open("./test_files/08_file_crash_test", O_RDONLY);
+	if (fd1 == -1)
+	{
+		printf("error open()");
+		return (0);
+	}
+	while (i != 0 && n != 0 && c != 0)
+	{
+		printf("ret fd1 [%d] = ", (i = get_next_line(fd1, &line)));
+		printf("|%s|\n", line);
+		free(line);
+		printf("ret fd2 [%d] = ", (n = get_next_line(fd2, &line)));
+		printf("|%s|\n", line);
+		free(line);
+		printf("ret stand [%d] = ", (c = get_next_line(0, &line)));
+		printf("|%s|\n", line);
+		free(line);
+	}
+	return (0);
+}
